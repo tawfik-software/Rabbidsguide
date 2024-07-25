77103480ea2d1d86be97374ab0554d260ecccca3
hamza bouchtarate <hamza.bouchtarate60@gmail.com>
2024-07-25 10:23:18 +0100

    profile

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,25 +1,30 @@
-import { View, Text, Image, TouchableOpacity, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
-import { Entypo, AntDesign} from '@expo/vector-icons';
+import { Entypo, AntDesign } from "@expo/vector-icons";
 
 const profile = () => {
   return (
     <SafeAreaView>
-      <View
-        className="items-center mt-7 justify-center"
-      >
-      <Image 
-        source={require('../../assets/images/firstimage.png')}
-        className="h-32 w-32 rounded-full"
+      <View className="items-center mt-7 justify-center">
+        <Image
+          source={require("../../assets/images/firstimage.png")}
+          className="h-32 w-32 rounded-full"
         />
 
-          <Text className="font-mRegular text-sm p-5">Rayman Raving Rabbids</Text>
+        <Text className="font-mRegular text-sm p-5">Rayman Raving Rabbids</Text>
 
+        <View className="px-4">
           <View className="mt-4">
-          <Text className="text-xl font-mRegular right-32">Support us</Text>
+            <Text className="text-xl font-mRegular right-32">Support us</Text>
           </View>
 
-        <View>
+          {/* <View>
         <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
         <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
               <View className="left-7 flex-row">
@@ -36,48 +41,107 @@ const profile = () => {
                 /> 
               </View>
         </TouchableOpacity>
+        </View> */}
+          <TouchableOpacity className="bg-[#f0eded] w-full rounded-2xl flex-row items-center justify-between px-6 py-3 my-4">
+            <View className="flex-row items-center gap-x-2">
+              <Entypo
+                name="map"
+                size={24}
+                color="black"
+                style={{ right: 15 }}
+              />
+              <Text className="text-black text-md font-mRegular text-lg">
+                Rate Us
+              </Text>
+            </View>
+            <View>
+              <AntDesign name="arrowright" size={16} color="black" />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-[#f0eded] w-full rounded-2xl flex-row items-center justify-between px-6 py-3 my-4">
+            <View className="flex-row items-center gap-x-2">
+              <Entypo
+                name="map"
+                size={24}
+                color="black"
+                style={{ right: 15 }}
+              />
+              <Text className="text-black text-md font-mRegular text-lg">
+                Rate Us
+              </Text>
+            </View>
+            <View>
+              <AntDesign name="arrowright" size={16} color="black" />
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity className="bg-[#f0eded] w-full rounded-2xl flex-row items-center justify-between px-6 py-3 my-4">
+            <View className="flex-row items-center gap-x-2">
+              <Entypo
+                name="map"
+                size={24}
+                color="black"
+                style={{ right: 15 }}
+              />
+              <Text className="text-black text-md font-mRegular text-lg">
+                Rate Us
+              </Text>
+            </View>
+            <View>
+              <AntDesign name="arrowright" size={16} color="black" />
+            </View>
+          </TouchableOpacity>
         </View>
 
         {/* 2 */}
 
         <View>
-        <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
-        <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
+          <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
+            <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
               <View className="left-3 flex-row">
-                <Entypo name="map" size={24} color="black" style={{right: 15}} />
+                <Entypo
+                  name="map"
+                  size={24}
+                  color="black"
+                  style={{ right: 15 }}
+                />
                 <Text className="text-black text-md font-mRegular text-lg">
-                  Contact Us  Share with friends
+                  Contact Us Share with friends
                 </Text>
               </View>
-                <AntDesign
-                  name="arrowright"
-                  size={16}
-                  color="black"
-                  style={{ position: "center", bottom: 0, left: 250 }}
-                /> 
-              </View>
-        </TouchableOpacity>
+              <AntDesign
+                name="arrowright"
+                size={16}
+                color="black"
+                style={{ position: "center", bottom: 0, left: 250 }}
+              />
+            </View>
+          </TouchableOpacity>
         </View>
-        
+
         {/* 3 */}
 
         <View>
-        <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
-        <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
+          <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
+            <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
               <View className="left-7 flex-row">
-                <Entypo name="map" size={24} color="black" style={{right: 15}} />
+                <Entypo
+                  name="map"
+                  size={24}
+                  color="black"
+                  style={{ right: 15 }}
+                />
                 <Text className="text-black text-md font-mRegular text-lg">
                   Share with friends
                 </Text>
               </View>
-                <AntDesign
-                  name="arrowright"
-                  size={16}
-                  color="black"
-                  style={{ position: "center", bottom: 0, left: 250 }}
-                /> 
-              </View>
-        </TouchableOpacity>
+              <AntDesign
+                name="arrowright"
+                size={16}
+                color="black"
+                style={{ position: "center", bottom: 0, left: 250 }}
+              />
+            </View>
+          </TouchableOpacity>
         </View>
 
         <View className="mt-11">
@@ -87,65 +151,79 @@ const profile = () => {
 
       <View>
         <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
-        <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
-              <View className="left-7 flex-row">
-                <Entypo name="map" size={24} color="black" style={{right: 15}} />
-                <Text className="text-black text-md font-mRegular text-lg">
+          <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
+            <View className="left-7 flex-row">
+              <Entypo
+                name="map"
+                size={24}
+                color="black"
+                style={{ right: 15 }}
+              />
+              <Text className="text-black text-md font-mRegular text-lg">
                 About us
-                </Text>
-              </View>
-                <AntDesign
-                  name="arrowright"
-                  size={16}
-                  color="black"
-                  style={{ position: "center", bottom: 0, left: 250 }}
-                /> 
-              </View>
+              </Text>
+            </View>
+            <AntDesign
+              name="arrowright"
+              size={16}
+              color="black"
+              style={{ position: "center", bottom: 0, left: 250 }}
+            />
+          </View>
         </TouchableOpacity>
-        </View>
+      </View>
 
-        {/* 2 */}
+      {/* 2 */}
 
-        <View>
+      <View>
         <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
-        <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
-              <View className="left-3 flex-row">
-                <Entypo name="map" size={24} color="black" style={{right: 15}} />
-                <Text className="text-black text-md font-mRegular text-lg">
+          <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
+            <View className="left-3 flex-row">
+              <Entypo
+                name="map"
+                size={24}
+                color="black"
+                style={{ right: 15 }}
+              />
+              <Text className="text-black text-md font-mRegular text-lg">
                 Terms and Conditions
-                </Text>
-              </View>
-                <AntDesign
-                  name="arrowright"
-                  size={16}
-                  color="black"
-                  style={{ position: "center", bottom: 0, left: 250 }}
-                /> 
-              </View>
+              </Text>
+            </View>
+            <AntDesign
+              name="arrowright"
+              size={16}
+              color="black"
+              style={{ position: "center", bottom: 0, left: 250 }}
+            />
+          </View>
         </TouchableOpacity>
-        </View>
-        
-        {/* 3 */}
+      </View>
 
-        <View>
+      {/* 3 */}
+
+      <View>
         <TouchableOpacity className="flex-row items-center space-x-2 pb-2 mx-14 w-screen">
-        <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
-              <View className="left-7 flex-row">
-                <Entypo name="map" size={24} color="black" style={{right: 15}} />
-                <Text className="text-black text-md font-mRegular text-lg">
+          <View className="flex-row ml-2 flex-1 space-x-2 bg-[#f0eded] rounded-2xl p-3">
+            <View className="left-7 flex-row">
+              <Entypo
+                name="map"
+                size={24}
+                color="black"
+                style={{ right: 15 }}
+              />
+              <Text className="text-black text-md font-mRegular text-lg">
                 Privacy Policy
-                </Text>
-              </View>
-                <AntDesign
-                  name="arrowright"
-                  size={16}
-                  color="black"
-                  style={{ position: "center", bottom: 0, left: 250 }}
-                /> 
-              </View>
+              </Text>
+            </View>
+            <AntDesign
+              name="arrowright"
+              size={16}
+              color="black"
+              style={{ position: "center", bottom: 0, left: 250 }}
+            />
+          </View>
         </TouchableOpacity>
-        </View>
-
+      </View>
     </SafeAreaView>
   );
 };
