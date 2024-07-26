fa3c647efa3191fa8fae3578ff359253badb9cc8
hamza bouchtarate <hamza.bouchtarate60@gmail.com>
2024-07-26 16:02:24 +0100

    ios size

--- app/user.ubisoft.jsx
+++ app/user.ubisoft.jsx
@@ -1,64 +1,70 @@
-import { View, Image, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
-import { Card } from 'react-native-elements'
+import { View, Image, Text, SafeAreaView, ScrollView } from "react-native";
+import React from "react";
+import { Card } from "react-native-elements";
 
 const userubisoft = () => {
   return (
     <SafeAreaView>
       <ScrollView>
-
-      <View className="flex-1">
-          <Image 
-          source={require('../assets/images/bodyarticles.png')}
-          className="h-44 w-96 rounded-lg top-3 left-1"
+        <View className="flex-1">
+          <Image
+            source={require("../assets/images/bodyarticles.png")}
+            className="h-44 w-96 rounded-lg top-3 left-1"
           />
 
           <View className="top-4 left-1">
-          <Text className="text-xs leading-4 w-auto">
-          Ubisoft, I say this respectfully: please use your amazing IPs in more creative 
-          ways that appeal to your community rather than alienating your fanbase through,
-           ways that appeal to your community rather, Ubisoft, I say this respectfully: please use your 
-           amazing IPs in more creative ways that appeal to your community rather than alienating your fanbase through,
-          ways that appeal to your community rather...
-          </Text>
-          <Text className="text-xs leading-4 w-auto">
-            Ubisoft, I say this respectfully: please use your amazing IPs in more creative ways that appeal 
-            to your community rather than alienating your fanbase through, ways that appeal to your community 
-            rather, Ubisoft, I say this respectfully: please use your amazing IPs in more creative ways 
-            that appeal to your community rather than alienating your fanbase through, 
-            ways that appeal to your community rather...community rather than alienating your fanbase through, 
-            ways that appeal to your community rather
-          </Text>
+            <Text className="text-xs leading-4 w-auto">
+              Ubisoft, I say this respectfully: please use your amazing IPs in
+              more creative ways that appeal to your community rather than
+              alienating your fanbase through, ways that appeal to your
+              community rather, Ubisoft, I say this respectfully: please use
+              your amazing IPs in more creative ways that appeal to your
+              community rather than alienating your fanbase through, ways that
+              appeal to your community rather...
+            </Text>
+            <Text className="text-xs leading-4 w-auto">
+              Ubisoft, I say this respectfully: please use your amazing IPs in
+              more creative ways that appeal to your community rather than
+              alienating your fanbase through, ways that appeal to your
+              community rather, Ubisoft, I say this respectfully: please use
+              your amazing IPs in more creative ways that appeal to your
+              community rather than alienating your fanbase through, ways that
+              appeal to your community rather...community rather than alienating
+              your fanbase through, ways that appeal to your community rather
+            </Text>
           </View>
 
           <View className="flex">
-        <View className="top-1">
-          <Image 
-          source={require('../assets/images/usernav.jpeg')}
-          className="h-44 w-96 rounded-lg top-3 left-1"
-          />
-        </View>
+            <View className="top-1">
+              <Image
+                source={require("../assets/images/usernav.jpeg")}
+                className="h-44 w-96 rounded-lg top-3 left-1"
+              />
+            </View>
 
-        <View className="top-4 left-1">
-          <Text className="text-xs leading-4 w-auto">
-            Ubisoft, I say this respectfully: please use your amazing IPs in more creative ways that appeal
-            to your community rather than alienating your fanbase through, ways that appeal to your community 
-            rather, Ubisoft, I say this respectfully: please use your amazing IPs in more creative ways
-            that appeal to your community rather than alienating your fanbase through,
-          ways that appeal to your community rather, Ubisoft, I say this respectfully: 
-            please use your amazing IPs in more creative ways that appeal to your community rather than alienating 
-            your fanbase through, ways that appeal to your community rather, Ubisoft, 
-            I say this respectfully: please use your amazing IPs in more creative ways
-             that appeal to your community rather than alienating your fanbase through,
-             ways that appeal to your community rather.
-          </Text>
+            <View className="top-4 left-1">
+              <Text className="text-xs leading-4 w-auto">
+                Ubisoft, I say this respectfully: please use your amazing IPs in
+                more creative ways that appeal to your community rather than
+                alienating your fanbase through, ways that appeal to your
+                community rather, Ubisoft, I say this respectfully: please use
+                your amazing IPs in more creative ways that appeal to your
+                community rather than alienating your fanbase through, ways that
+                appeal to your community rather, Ubisoft, I say this
+                respectfully: please use your amazing IPs in more creative ways
+                that appeal to your community rather than alienating your
+                fanbase through, ways that appeal to your community rather,
+                Ubisoft, I say this respectfully: please use your amazing IPs in
+                more creative ways that appeal to your community rather than
+                alienating your fanbase through, ways that appeal to your
+                community rather.
+              </Text>
+            </View>
+          </View>
         </View>
-        </View>
-  
-      </View>
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default userubisoft
+export default userubisoft;
